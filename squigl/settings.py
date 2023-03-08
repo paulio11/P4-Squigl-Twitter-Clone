1993c9a09c7fa5748dc732c99828a465f7ce35d4
Paul Young <youngjpj@gmail.com>
2023-03-08 20:00:48 +0000

    Remove favicons

--- squigl/settings.py
+++ squigl/settings.py
@@ -28,9 +28,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'squigl.herokuapp.com', 'squigl.co.uk']
+ALLOWED_HOSTS = ['localhost', 'squigl.herokuapp.com']
 
 
 # Application definition
