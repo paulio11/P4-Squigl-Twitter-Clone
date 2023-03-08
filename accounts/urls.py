f4ba15566ec099331ffb97dc99b528d4dc8acfa5
Paul Young <youngjpj@gmail.com>
2023-03-08 17:36:58 +0000

    Add edit profile view and url

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('signup/', views.SignUpView.as_view(), name='signup'),
+    path('edit-profile/<int:pk>', views.EditProfile.as_view(), name='edit_profile'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,27 @@
 # Django imports
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 
 # My imports
-from .forms import CustomUserCreationForm
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser
 
 
+# User sign up
 class SignUpView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
+
+
+# Edit user profile
+class EditProfile(UpdateView):
+    model = CustomUser
+    form_class = CustomUserChangeForm
+    template_name = 'edit-profile.html'
+
+    def get_success_url(self):
+        return reverse_lazy('user', kwargs={
+            'user': self.object.request.user
+        })
