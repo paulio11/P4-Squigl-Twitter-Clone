3396f15bd6c612a637aa3d7071d70d7c4899a002
Paul Young <youngjpj@gmail.com>
2023-03-08 17:02:22 +0000

    Add custom tag to check for user replies on post

---
+++ social/templatetags/social_extras.py
@@ -0,0 +1,18 @@
+# Django imports
+from django import template
+
+# My imports
+from ..models import Post, Reply
+
+
+register = template.Library()
+
+
+# Check if user has replied to a post
+@register.simple_tag
+def user_has_replied(user, post):
+    replies = Reply.objects.filter(post=post).filter(user=user).count()
+    if replies >= 1:
+        return True
+    else:
+        return False
