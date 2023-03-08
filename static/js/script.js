a174301beec5bbd64802502dd40d076f136467ec
Paul Young <youngjpj@gmail.com>
2023-03-08 16:52:33 +0000

    Add animated refresh feed icon to nav

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,13 @@
 // Follow user button
-$('#follow-btn:contains(Following)').mouseenter(function () {
+$('#follow-btn:contains(Following)').hover(function () {
     $(this).removeClass('btn-dark').addClass('btn-danger').text('Unfollow');
-}).mouseleave(function () {
+}, function () {
     $(this).removeClass('btn-danger').addClass('btn-dark').text('Following');
 })
+
+// Refresh feed icon spin
+$('#feed-link').hover(function () {
+    $('#feed-icon').addClass('fa-spin');
+}, function () {
+    $('#feed-icon').removeClass('fa-spin');
+});
