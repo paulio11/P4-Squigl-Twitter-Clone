d87538d851f03a68267c1135d72424b693a49df8
Paul Young <youngjpj@gmail.com>
2023-03-08 15:55:00 +0000

    Add feed view for followed user's posts

--- social/views.py
+++ social/views.py
@@ -1,6 +1,22 @@
+# Django imports
 from django.shortcuts import render
+from django.db.models import Q
+
+# My imports
+from .models import Post
 
 
 # Home
 def home(request):
     return render(request, 'home.html')
+
+
+# Feed (followed user's and own posts)
+def feed(request):
+    following = request.user.following
+    posts = Post.objects.filter(
+        Q(user__in=following.all()) | Q(user=request.user))
+
+    return render(request, 'feed.html', {
+        'posts': posts,
+    })
