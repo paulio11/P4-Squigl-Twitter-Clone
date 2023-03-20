7250a52ec463daeb841bf76ff5c46f0ff33be782
Paul Young <youngjpj@gmail.com>
2023-03-20 23:17:42 +0000

    Remove admin-action

--- accounts/admin.py
+++ accounts/admin.py
@@ -7,15 +7,9 @@ from django.contrib.auth.models import Group
 from .models import CustomUser
 
 
-@admin.action(description='Mark user as verified')
-def verify_user(modeladmin, request, queryset):
-    queryset.update(verified=True)
-
-
 class CustomUserAdmin(admin.ModelAdmin):
     list_display = ['username', 'name', 'email', 'verified', 'is_active']
     ordering = ['username']
-    actions = [verify_user]
 
 
 admin.site.register(CustomUser, CustomUserAdmin)
