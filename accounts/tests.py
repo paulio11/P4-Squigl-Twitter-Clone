5848aec45318ad693b369a75ab36e2b098f912b1
Paul Young <youngjpj@gmail.com>
2023-03-21 15:20:48 +0000

    Remove unnecessary selfs

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,6 +1,12 @@
-from django.test import TestCase
+# Django imports
+from django.test import TestCase, RequestFactory
 from django.urls import reverse
+
+# My imports
 from .models import CustomUser
+from .views import UserSettings
+
+# Other imports
 from model_bakery import baker
 
 
@@ -16,12 +22,11 @@ class CustomUserModelTests(TestCase):
     def test_followers_count(self):
         self.testuser2.following.set([self.testuser])
         self.testuser3.following.set([self.testuser])
-        self.assertEqual(CustomUser.objects.filter(
-            following=self.testuser.id).count(), 2)
+        self.assertEqual(self.testuser.followers_count(), 2)
 
     def test_following_count(self):
         self.testuser.following.set([self.testuser2, self.testuser3])
-        self.assertEqual(self.testuser.following.count(), 2)
+        self.assertEqual(self.testuser.following_count(), 2)
 
 
 # View Tests
@@ -51,19 +56,19 @@ class ChangePasswordTests(TestCase):
         self.url = reverse('password')
 
     def test_post(self):
-        self.response = self.client.post(self.url, {
+        response = self.client.post(self.url, {
             'old_password': 'testpass',
             'new_password1': 'newtestpass',
             'new_password2': 'newtestpass',
         })
-        self.assertEqual(self.response.status_code, 302)
+        self.assertEqual(response.status_code, 302)
         self.testuser.refresh_from_db()
         self.assertTrue(self.testuser.check_password('newtestpass'))
 
     def test_get(self):
-        self.response = self.client.get(self.url)
-        self.assertEqual(self.response.status_code, 200)
-        self.assertTemplateUsed(self.response, 'accounts/password.html')
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'accounts/password.html')
 
 
 class DeleteAccountTests(TestCase):
@@ -75,13 +80,31 @@ class DeleteAccountTests(TestCase):
         self.url = reverse('delete_account')
 
     def test_post(self):
-        self.response = self.client.post(self.url)
-        self.assertEqual(self.response.status_code, 302)
+        response = self.client.post(self.url)
+        self.assertEqual(response.status_code, 302)
         self.assertEqual(CustomUser.objects.count(), 0)
 
     def test_error(self):
-        self.response = self.client.get(self.url)
-        self.assertEqual(self.response.status_code, 200)
-        self.assertTemplateUsed(self.response, 'error.html')
-        self.assertContains(
-            self.response, 'You do not have permission to do this.')
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'error.html')
+        self.assertContains(response, 'You do not have permission to do this.')
+
+
+class UserSettingsTests(TestCase):
+
+    def setUp(self):
+        self.testuser = CustomUser.objects.create_user(
+            username='testuser', password='testpass')
+        self.testuser2 = baker.make('accounts.CustomUser')
+        self.client.login(username='testuser', password='testpass')
+
+    def test_get_queryset(self):
+        request = RequestFactory().get(
+            f'/accounts/settings/{self.testuser.pk}')
+        view = UserSettings()
+        view.request = request
+        view.request.user = self.testuser
+        qs = view.get_queryset()
+        # self.assertQuerysetEqual(qs, CustomUser.objects.all())
+        self.assertEqual(qs.count(), 1)
