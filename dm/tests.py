71cf9f8a7d85272ec475ae29d370f55c27147a8d
Paul Young <youngjpj@gmail.com>
2023-03-21 13:07:38 +0000

    Add dm.models tests

--- dm/tests.py
+++ dm/tests.py
@@ -1,3 +1,32 @@
+# Django imports
 from django.test import TestCase
 
-# Create your tests here.
+# My imports
+from .models import Message
+from accounts.models import CustomUser
+
+# Other imports
+from model_bakery import baker
+
+
+class MessageModelTests(TestCase):
+
+    def setUp(self):
+        self.testuser = baker.make(
+            'accounts.CustomUser',
+            username='sender'
+        )
+        self.testuser2 = baker.make(
+            'accounts.CustomUser',
+            username='recipient'
+        )
+        self.message = baker.make(
+            'dm.Message',
+            sender=self.testuser,
+            recipient=self.testuser2,
+        )
+
+    def test_str(self):
+        self.assertEqual(
+            str(self.message), f'Message from: sender, to: recipient'
+        )
