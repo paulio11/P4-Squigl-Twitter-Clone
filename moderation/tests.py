7bec9dcaa2ec6464cb5ab1933c2e78af5201dca5
Paul Young <youngjpj@gmail.com>
2023-03-21 15:33:21 +0000

    Add mod_count test

--- moderation/tests.py
+++ moderation/tests.py
@@ -1,3 +1,25 @@
+# Django imports
 from django.test import TestCase
 
-# Create your tests here.
+# My imports
+from .templatetags.moderation_extras import mod_count
+
+# Other imports
+from model_bakery import baker
+
+
+# Template tag tests
+
+class ModCountTests(TestCase):
+
+    def setUp(self):
+        self.testuser = baker.make('accounts.CustomUser')
+        self.testuser2 = baker.make('accounts.CustomUser')
+        self.post = baker.make('social.Post')
+        self.reply = baker.make('social.Reply')
+        self.message = baker.make('dm.Message', reported=True)
+        self.post.reported.set([self.testuser])
+        self.reply.reported.set([self.testuser])
+
+    def test_mod_count(self):
+        self.assertEqual(mod_count(), 3)
