cef9561eb6742409ca0d526a40b78b539f3ad237
Paul Young <youngjpj@gmail.com>
2023-03-21 20:04:16 +0000

    Add final tests for all moderation views

--- moderation/tests.py
+++ moderation/tests.py
@@ -1,8 +1,12 @@
 # Django imports
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
 # My imports
 from .templatetags.moderation_extras import mod_count
+from social.models import Post, Reply
+from dm.models import Message
+from accounts.models import CustomUser
 
 # Other imports
 from model_bakery import baker
@@ -23,3 +27,338 @@ class ModCountTests(TestCase):
 
     def test_mod_count(self):
         self.assertEqual(mod_count(), 3)
+
+
+# Views tests
+
+class ModerationTests(TestCase):
+
+    def setUp(self):
+        self.testmod = CustomUser.objects.create_user(
+            username='testmod',
+            password='testpass',
+            email='1@1.com',
+            is_staff=True,
+        )
+        self.testuser = CustomUser.objects.create_user(
+            username='testuser',
+            password='testpass',
+            email='1@2.com',
+            is_staff=False,
+        )
+        self.client = Client()
+        self.url = reverse('moderation')
+
+    def test_login_required(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 302)
+        self.assertTemplateUsed('registration/login.html')
+
+    def test_login_as_normal_user(self):
+        self.client.login(username='testuser', password='testpass')
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'error.html')
+        self.assertContains(response, 'Only Squigl moderators can view this page.')
+
+    def test_moderation(self):
+        self.client.login(username='testmod', password='testpass')
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'moderation/moderation.html')
+
+
+class ModDeletePostTests(TestCase):
+
+    def setUp(self):
+        self.testmod = CustomUser.objects.create_user(
+            username='testmod',
+            password='testpass',
+            email='1@1.com',
+            is_staff=True,
+        )
+        self.testuser = CustomUser.objects.create_user(
+            username='testuser',
+            password='testpass',
+            email='1@2.com',
+            is_staff=False,
+        )
+        self.post = baker.make('social.Post')
+        self.client = Client()
+        self.url = reverse('mod_delete_post', args={self.post.id})
+
+    def test_login_required(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 302)
+        self.assertTemplateUsed('registration/login.html')
+
+    def test_login_as_normal_user(self):
+        self.client.login(username='testuser', password='testpass')
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'error.html')
+        self.assertContains(response, 'You can not delete this message post you are not a Squigl moderator.')
+
+    def test_mod_delete(self):
+        self.client.login(username='testmod', password='testpass')
+        response = self.client.get(self.url)
+        self.post.user.refresh_from_db()
+        self.assertEquals(Post.objects.count(), 0)
+        self.assertEquals(self.post.user.strikes, 1)
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, '/mod/')
+
+
+class ModDeleteReplyTests(TestCase):
+
+    def setUp(self):
+        self.testmod = CustomUser.objects.create_user(
+            username='testmod',
+            password='testpass',
+            email='1@1.com',
+            is_staff=True,
+        )
+        self.testuser = CustomUser.objects.create_user(
+            username='testuser',
+            password='testpass',
+            email='1@2.com',
+            is_staff=False,
+        )
+        self.post = baker.make('social.Post')
+        self.reply = baker.make('social.Reply')
+        self.client = Client()
+        self.url = reverse('mod_delete_reply', args={self.reply.id})
+
+    def test_login_required(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 302)
+        self.assertTemplateUsed('registration/login.html')
+
+    def test_login_as_normal_user(self):
+        self.client.login(username='testuser', password='testpass')
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'error.html')
+        self.assertContains(response, 'You can not delete this reply because you are not a Squigl moderator.')
+
+    def test_mod_delete(self):
+        self.client.login(username='testmod', password='testpass')
+        response = self.client.get(self.url)
+        self.reply.user.refresh_from_db()
+        self.assertEquals(Reply.objects.count(), 0)
+        self.assertEquals(self.reply.user.strikes, 1)
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, '/mod/')
+
+
+class ModDeleteMessageTests(TestCase):
+
+    def setUp(self):
+        self.testmod = CustomUser.objects.create_user(
+            username='testmod',
+            password='testpass',
+            email='1@1.com',
+            is_staff=True,
+        )
+        self.testuser = CustomUser.objects.create_user(
+            username='testuser',
+            password='testpass',
+            email='1@2.com',
+            is_staff=False,
+        )
+        self.message = baker.make('dm.Message')
+        self.client = Client()
+        self.url = reverse('mod_delete_msg', args={self.message.id})
+
+    def test_login_required(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 302)
+        self.assertTemplateUsed('registration/login.html')
+
+    def test_login_as_normal_user(self):
+        self.client.login(username='testuser', password='testpass')
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'error.html')
+        self.assertContains(response, 'You can not delete this message because you are not a Squigl moderator.')
+
+    def test_mod_delete(self):
+        self.client.login(username='testmod', password='testpass')
+        response = self.client.get(self.url)
+        self.message.sender.refresh_from_db()
+        self.assertEquals(Message.objects.count(), 0)
+        self.assertEquals(self.message.sender.strikes, 1)
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, '/mod/')
+
+
+class PostIsOkayTests(TestCase):
+
+    def setUp(self):
+        self.testmod = CustomUser.objects.create_user(
+            username='testmod',
+            password='testpass',
+            email='1@1.com',
+            is_staff=True,
+        )
+        self.testuser = CustomUser.objects.create_user(
+            username='testuser',
+            password='testpass',
+            email='1@2.com',
+            is_staff=False,
+        )
+        self.post = baker.make('social.Post')
+        self.post.reported.set([self.testuser])
+        self.client = Client()
+        self.url = reverse('mod_post_okay', args={self.post.id})
+
+    def test_login_required(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 302)
+        self.assertTemplateUsed('registration/login.html')
+
+    def test_login_as_normal_user(self):
+        self.client.login(username='testuser', password='testpass')
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'error.html')
+        self.assertContains(response, 'You can not okay this post because you are not a Squigl moderator.')
+
+    def test_post_okay(self):
+        self.client.login(username='testmod', password='testpass')
+        response = self.client.get(self.url)
+        self.post.refresh_from_db()
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, '/mod/')
+        self.assertEqual(self.post.reported.count(), 0)
+
+
+class ReplyIsOkayTests(TestCase):
+
+    def setUp(self):
+        self.testmod = CustomUser.objects.create_user(
+            username='testmod',
+            password='testpass',
+            email='1@1.com',
+            is_staff=True,
+        )
+        self.testuser = CustomUser.objects.create_user(
+            username='testuser',
+            password='testpass',
+            email='1@2.com',
+            is_staff=False,
+        )
+        self.post = baker.make('social.Post')
+        self.reply = baker.make('social.Reply')
+        self.reply.reported.set([self.testuser])
+        self.client = Client()
+        self.url = reverse('mod_reply_okay', args={self.reply.id})
+
+    def test_login_required(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 302)
+        self.assertTemplateUsed('registration/login.html')
+
+    def test_login_as_normal_user(self):
+        self.client.login(username='testuser', password='testpass')
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'error.html')
+        self.assertContains(response, 'You can not okay this reply because you are not a Squigl moderator.')
+
+    def test_reply_okay(self):
+        self.client.login(username='testmod', password='testpass')
+        response = self.client.get(self.url)
+        self.reply.refresh_from_db()
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, '/mod/')
+        self.assertEqual(self.reply.reported.count(), 0)
+
+
+class MessageIsOkayTests(TestCase):
+
+    def setUp(self):
+        self.testmod = CustomUser.objects.create_user(
+            username='testmod',
+            password='testpass',
+            email='1@1.com',
+            is_staff=True,
+        )
+        self.testuser = CustomUser.objects.create_user(
+            username='testuser',
+            password='testpass',
+            email='1@2.com',
+            is_staff=False,
+        )
+        self.message = baker.make('dm.Message', reported=True)
+        self.client = Client()
+        self.url = reverse('mod_msg_okay', args={self.message.id})
+
+    def test_login_required(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 302)
+        self.assertTemplateUsed('registration/login.html')
+
+    def test_login_as_normal_user(self):
+        self.client.login(username='testuser', password='testpass')
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'error.html')
+        self.assertContains(response, 'You can not okay this message because you are not a Squigl moderator.')
+
+    def test_message_okay(self):
+        self.client.login(username='testmod', password='testpass')
+        response = self.client.get(self.url)
+        self.message.refresh_from_db()
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, '/mod/')
+        self.assertFalse(self.message.reported)
+
+
+class BanUserTests(TestCase):
+
+    def setUp(self):
+        self.testmod = CustomUser.objects.create_user(
+            username='testmod',
+            password='testpass',
+            email='1@1.com',
+            is_staff=True,
+        )
+        self.testuser = CustomUser.objects.create_user(
+            username='testuser',
+            password='testpass',
+            email='1@2.com',
+            is_staff=False,
+        )
+        self.client = Client()
+        self.url = reverse('ban_user', args={self.testuser.id})
+
+    def test_login_required(self):
+        response = self.client.get(self.url)
+        self.assertEquals(response.status_code, 302)
+        self.assertTemplateUsed('registration/login.html')
+
+    def test_login_as_normal_user(self):
+        self.client.login(username='testuser', password='testpass')
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'error.html')
+        self.assertContains(response, 'You can not ban this user because you are not a Squigl moderator.')
+
+    def test_ban_user(self):
+        self.client.login(username='testmod', password='testpass')
+        response = self.client.get(self.url)
+        self.testuser.refresh_from_db()
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, '/mod/')
+        self.assertFalse(self.testuser.is_active)
+
+    def test_unban_user(self):
+        self.testuser.is_active = False
+        self.testuser.save()
+        self.client.login(username='testmod', password='testpass')
+        response = self.client.get(self.url)
+        self.testuser.refresh_from_db()
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, '/mod/')
+        self.assertTrue(self.testuser.is_active)
